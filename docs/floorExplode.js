8bd3b44e8d855b54280e3ce9292a770570df13d5
Adam Nagy <adam_e_nagy@hotmail.com>
2024-06-17 12:31:53 +0100

    multi-model

--- docs/floorExplode.js
+++ docs/floorExplode.js
@@ -1,52 +1,36 @@
 // floorExplode.js
 
-function floorExplode(viewer, scale, excludedFragIds, model) {
-
-    model = model || viewer.model
-    var svf = model.getData();
-    var mc = model.getVisibleBounds(true).center();
-    var fragList = model.getFragmentList();
-    var pt = new THREE.Vector3();
-
-    //Input scale is in the range 0-1, where 0
-    //means no displacement, and 1 maximum reasonable displacement.
-    scale *= 2;
-
-    var boxes = fragList.fragments.boxes;
-    var nbFrags = fragList.getCount();
-    for (var fragId = 0; fragId < nbFrags; ++fragId) {
-
-        if (scale == 0) {
-
-            fragList.updateAnimTransform(fragId);
-
-        } else {
-
-            var box_offset = fragId * 6;
-            var cz = Math.floor(boxes[box_offset + 2] / 10) * 10; // + boxes[box_offset + 5];
-            //cz = scale * (cz - mc.z);
-            pt.z = cz * scale * 4;
-
-            //pt.z = mc.z;
-
-            fragList.updateAnimTransform(fragId, null, null, pt);
-        }
+function getFloorOffsetZ(fragMiddleZ, scale, levels, globalOffsetZ) {
+  for (const key in levels) {
+    const level = levels[key];
+    // If the middle of the fragment is below the top of the level
+    if (fragMiddleZ < level.elevation + level.height - globalOffsetZ) {
+      return key * scale;
     }
+  }
 }
 
+function floorExplode(viewer, scale, levels) {
+  // We'll just use the AEC Model Data of the first model
+  // If multiple models are loaded, they probably have that in common
+  // You could also use the AEC Model Data of a specific model
+  for (const model of viewer.getAllModels()) {
+    const fragList = model.getFragmentList();
+    let pt = new THREE.Vector3();
 
-// animate.js
-function circ(timeFraction) { return 1 - Math.sin(Math.acos(timeFraction)) }
-function makeEaseOut(timing) { return function(timeFraction) {return 1 - timing(1 - timeFraction)}}
-function animate({timing, draw, duration}) {
-  let start = performance.now();
-  requestAnimationFrame(function animate(time) {
-    let timeFraction = (time - start) / duration;
-    if (timeFraction > 1) timeFraction = 1;
-    let progress = timing(timeFraction);
-    draw(progress); // draw it
-    if (timeFraction < 1) {
-      requestAnimationFrame(animate);
+    const boxes = fragList.fragments.boxes;
+    for (const fragId in fragList.fragments.fragId2dbId) {
+      const fragIdInt = parseInt(fragId);
+      if (scale == 0) {
+        fragList.updateAnimTransform(fragIdInt);
+      } else {
+        const box_offset = fragIdInt * 6;
+
+        const fragMiddleZ = (boxes[box_offset + 2] + boxes[box_offset + 5]) / 2;
+        pt.z = getFloorOffsetZ(fragMiddleZ, scale, levels, model.myData.globalOffset.z);
+
+        fragList.updateAnimTransform(fragIdInt, null, null, pt);
+      }
     }
-  });
+  }
 }
