8bca441e3ff7674a1538444e890f631c8e79eedd
Adam Nagy <adam_e_nagy@hotmail.com>
2024-06-17 13:29:41 +0100

    use client credentials

--- docs/app.js
+++ docs/app.js
@@ -1,56 +1,79 @@
-const accessToken =
-  "eyJhbGciOiJSUzI1NiIsImtpZCI6IjY0RE9XMnJoOE9tbjNpdk1NU0xlNGQ2VHEwUV9SUzI1NiIsInBpLmF0bSI6ImFzc2MifQ.eyJzY29wZSI6WyJkYXRhOnJlYWQiLCJkYXRhOndyaXRlIiwiZGF0YTpjcmVhdGUiLCJidWNrZXQ6cmVhZCIsImJ1Y2tldDpkZWxldGUiLCJidWNrZXQ6dXBkYXRlIiwiYnVja2V0OmNyZWF0ZSJdLCJjbGllbnRfaWQiOiJyR20wbU85alZTc0QyeUJFRGs5TVJ0WFFUd3NhNjF5MCIsImlzcyI6Imh0dHBzOi8vZGV2ZWxvcGVyLmFwaS5hdXRvZGVzay5jb20iLCJhdWQiOiJodHRwczovL2F1dG9kZXNrLmNvbSIsImp0aSI6IlpWQTBTVWRZSkxVOTZBc2lKQ3lIQzZxUHZXbVJtUlVpd3RtNWNRSjBTTWJzNGtZQm9Oem8yN21OYTNvNGRQbDQiLCJleHAiOjE3MTg2MjUyNTV9.ugXO631gMPzf7x9PPLJRxQAYBEWCd6mkBBmzycKZu4xaxDsL6yzFel5zLyG7PhxsFNT68jAvpaMl4MCYkaGmVQuWC7eYWwMcj2cwvviPpqaUmdJbcIS9rkcVUAsGggh4gSiNPWwjZdfjQkFtytSVoDHn7wW27udrdtynUxKQMUYRxt-dajWJpMhPPQ4-zQlFJKcApAhPj9qUlVQWFjLxfIovq8q66ou3wKcgTVZrwkpihaZ1FeXKHmGubYsngH1JS35BfcgVq-CkiPpx1vkuWLzlwNNlM28kSqeng0InG-xF88JmStM6wRDbf3TtoUwf1jAMwzdCWo7rrIBs4mTsPA";
-const urn =
-  "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6YWRhbV8yMDE5MDMwNzIvcmFjX2Jhc2ljX3NhbXBsZV9wcm9qZWN0LnJ2dA";
-const urn2 =
-  "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6YWRhbV8yMDE5MDMwNzIvcnN0YmFzaWNzYW1wbGVwcm9qZWN0LnJ2dA";
+const clientId = "";
+// This is only for testing purposes. In a real-world application you should never expose your client secret
+// to the client side code. You should always use a server side code to get the access token.
+const clientSecret = "";
+// Can get sample models from here: 
+// https://help.autodesk.com/view/RVT/2024/ENU/?guid=GUID-61EF2F22-3A1F-4317-B925-1E85F138BE88
+const urn = "";
+const urn2 = "";
 const explodeScale = 2;
 
 let viewer;
-var aecData;
+let aecData;
+
+async function getAccessToken(callback) {
+  const response = await fetch(
+    "https://developer.api.autodesk.com/authentication/v2/token",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials&scope=bucket:read data:read data:create data:write viewables:read data:search`,
+    }
+  );
+
+  const data = await response.json();
+  callback(data.access_token, data.expires_in);
+}
 
 function loadModel(urn) {
   options = {
     useADP: false,
     env: "AutodeskProduction",
-    accessToken: accessToken,
+    getAccessToken: getAccessToken,
     isAEC: true,
   };
-  viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById("viewer"));
-  Autodesk.Viewing.Initializer(options);
-  Autodesk.Viewing.Document.load(`urn:${urn}`, async (doc) => {
-    doc.downloadAecModelData((data) => {
-      aecData = data;
-      aecData.height =
-        aecData.levels[aecData.levels.length - 1].elevation -
-        aecData.levels[0].elevation;
-      console.log(aecData.levels);
-    });
-
-    const geometries = doc.getRoot().search({ type: "geometry", role: "3d" });
-
+  
+  Autodesk.Viewing.Initializer(options, () => {
+    viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById("viewer"));
     viewer.start();
     viewer.setTheme("light-theme");
+    Autodesk.Viewing.Document.load(`urn:${urn}`, (doc) => {
+      const geometries = doc.getRoot().search({ type: "geometry", role: "3d" });
+      viewer.loadDocumentNode(doc, geometries[0]);
+    });
+    Autodesk.Viewing.Document.load(`urn:${urn}`, async (doc) => {
+      const geometries = doc.getRoot().search({ type: "geometry", role: "3d" });
 
-    let model = await viewer.loadDocumentNode(doc, geometries[0]);
+      viewer.start();
+      viewer.setTheme("light-theme");
 
-    if (urn2) {
-      Autodesk.Viewing.Document.load(`urn:${urn2}`, async (doc) => {
-        doc.downloadAecModelData();
+      let model = await viewer.loadDocumentNode(doc, geometries[0]);
 
-        const geometries = doc
-          .getRoot()
-          .search({ type: "geometry", role: "3d" });
-
-        viewer.start();
-        viewer.setTheme("light-theme");
-
-        viewer.loadDocumentNode(doc, geometries[0], {
-          keepCurrentModels: true,
-          globalOffset: model.myData.globalOffset,
-        });
+      doc.downloadAecModelData((data) => {
+        aecData = data;
+        aecData.height =
+          aecData.levels[aecData.levels.length - 1].elevation -
+          aecData.levels[0].elevation;
+        console.log(aecData.levels);
       });
-    }
+
+      if (urn2) {
+        Autodesk.Viewing.Document.load(`urn:${urn2}`, async (doc) => {
+          doc.downloadAecModelData();
+
+          const geometries = doc
+            .getRoot()
+            .search({ type: "geometry", role: "3d" });
+
+          viewer.loadDocumentNode(doc, geometries[0], {
+            keepCurrentModels: true,
+            globalOffset: model.myData.globalOffset,
+          });
+        });
+      }
+    });
   });
 }
 
